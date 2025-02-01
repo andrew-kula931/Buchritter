99551e1a8c09a10addc8ee153ec81a4a1023d73e
Andrew Kula <arkula931@gmail.com>
2025-02-01 11:47:12 -0600

    add css elements to doc page

--- buchritter/src/pages/document_page.tsx
+++ buchritter/src/pages/document_page.tsx
@@ -1,14 +1,39 @@
 import Link from "next/link";
 import { useState } from "react";
-import { ArrowLeft } from "lucide-react";
+import styles from './doc.module.css';
 
 import "../app/globals.css";
+
+const docStyle = {
+  document: {
+    width: "800px",
+    minHeight: "1060px",
+    margin: "20px auto",
+    padding: "20px",
+    backgroundColor: "rgb(66, 66, 66)",
+    borderRadius: "10px",
+    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+    fontFamily: "Arial, sans-serif",
+    lineHeight: "1.6",
+  },
+  title: {
+    fontSize: "24px",
+    marginBottom: "10px",
+    backgroundColor: "rgb(66, 66, 66)",
+    borderRadius: "10px",
+    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+  },
+};
 
 export default function DocumentPage() {
   return (
     <div>
       <div className="navBar">
-        <h1>Hello from the document!</h1>
+        <textarea
+          className={styles.textareaDefault}
+          style={docStyle.title}
+          defaultValue="Doc Name"
+        />
         <Link href="/">Click here to go back</Link>
       </div>
       <Canvas />
@@ -16,50 +41,25 @@ export default function DocumentPage() {
   );
 }
 
-//Document Page
 function Canvas() {
-  const docStyle = {
-    document: {
-      width: "800px",
-      minHeight: "1060px",
-      margin: "20px auto",
-      padding: "20px",
-      backgroundColor: "rgb(66, 66, 66)",
-      borderRadius: "10px",
-      boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-      fontFamily: "Arial, sans-serif",
-      lineHeight: "1.6",
-    },
-    title: {
-      fontSize: "24px",
-      marginBottom: "10px",
-      backgroundColor: "rgb(66, 66, 66)",
-      borderRadius: "10px",
-      boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-      border: "1px solid gray"
-    },
-  };
-
   const [text, setText] = useState("");
 
   return(
     <div style={docStyle.document}>
-      <input style={docStyle.title}/>
       <textarea
+        className={styles.textareaDefault}
         value={text}
         onChange={(e) => setText(e.target.value)}
-        rows={37}
+        rows={40}
         cols={40}
         style={{
           width: "760px", 
           padding: "10px", 
           borderRadius: "5px", 
-          border: "1px solid gray", 
           fontSize: "16px",
-          background: "rgb(66, 66, 66)"
+          background: "rgb(66, 66, 66)",
         }}
       />
-      <p>{text}</p>
     </div>
   );
 }
