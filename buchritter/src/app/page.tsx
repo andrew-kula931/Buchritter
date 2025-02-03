a1687fe6848e02d0f34bf0833f5edd9f3a426ca5
Andrew Kula <arkula931@gmail.com>
2025-02-03 08:50:09 -0600

    added the file list

--- buchritter/src/app/page.tsx
+++ buchritter/src/app/page.tsx
@@ -1,9 +1,15 @@
+"use client"
+
 import Link from "next/link";
+import { useState } from "react";
+import { useRef } from 'react';
 
 export default function Home() {
-
   return (
-    TitleBar()
+    <>
+      <TitleBar />
+      <DocFiles />
+    </>
   );
 }
 
@@ -12,6 +18,48 @@ function TitleBar() {
     <div className="navBar">
       <div style={{fontSize: 40}}>Buch Ritter</div>
       <Link href="/document-page">Go to Document</Link>
+    </div>
+  );
+}
+
+function File({ text, idx } : {text:string, idx:number}) {
+  const color = (idx % 2 === 0) ? "bg-gray-700" : "bg-gray-600";
+  const linkRef = useRef<HTMLAnchorElement>(null);
+
+  const handleClick = () => {
+    if (linkRef.current) {
+      linkRef.current.click();
+    }
+  }
+
+  return (
+  <li>
+    <div className={`p-2 ${color} hover:bg-gray-500`} onClick={handleClick}>
+      <a href="/document-page" ref={linkRef} style={{ display: 'none'}}>To Doc</a>
+      {text}
+    </div>
+  </li>
+  );
+}
+
+function DocFiles() {
+  const [items, setItems] = useState(["Item 1"]);
+
+  const addItem = () => {
+    setItems([...items, `Item ${items.length + 1}`]);
+  }
+
+  return (
+    <div className="flex flex-auto justify-center">
+      <div className="flex flex-col pt-2 w-[95vw]">
+        <h2 className="text-[1.65rem] pb-2">Files</h2>
+        <ul>
+          {items.map((item, index) => (
+            <File key={index} text={item} idx={index}/>
+          ))}
+          <button className="p-2" onClick={addItem}>Add Item</button>
+        </ul>
+      </div>
     </div>
   );
 }
