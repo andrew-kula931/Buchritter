69faf54d297129070dc813dee260190e01c3364e
Andrew Kula <arkula931@gmail.com>
2025-05-18 10:24:46 -0500

    feat(create_review): finished skeleton

--- buchritter/src/app/review_list/create_review.tsx
+++ buchritter/src/app/review_list/create_review.tsx
@@ -1,10 +1,11 @@
 "use client"
 
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Slider from '@mui/material/Slider';
 import { Plus } from 'lucide-react';
 import { FaRegStar } from "react-icons/fa";
-import { useState } from 'react';
+import { addReview } from '@/server/api/review_req';
 
 type ModalProps = {
     isOpen: boolean;
@@ -30,7 +31,12 @@ export default function CreateReview() {
 
 function CreateModal({ isOpen, onClose}: ModalProps) {
     if (!isOpen) return null;
+    const [title, setTitle] = useState<string>();
+    const [summary, setSummary] = useState<string>();
     const [rating, setRating] = useState<number>(0);
+    const [review, setReview] = useState<string>();
+    const [selected, setSelected] = useState<string[]>(["Movie"]);
+    const tags = ["Movie", "Manga", "Book", "Show", "Journal", "Anime", "Comic"];
 
     const ratingChange = (event: Event, newValue: number) => {
         setRating(newValue);
@@ -38,21 +44,26 @@ function CreateModal({ isOpen, onClose}: ModalProps) {
 
     return (
         <div className="fixed inset-0 flex justify-center items-center z-50">
-            <div className="flex flex-col max-w-[900px] max-h-[500px] p-4 bg-gray-700">
+            <div className="flex flex-col max-w-[900px] max-h-[700px] p-4 bg-gray-700">
                 <p className="pb-2 text-xl">Create New Review</p>
+                <hr className="p-2 border-t border-gray-500"></hr>
                 <div className="flex flex-row space-x-4">
-                    <div className="flex flex-col">
+                    <div className="flex flex-col w-[100%]">
                         <p>Title</p>
-                        <TextField id='titleField' variant="outlined" multiline maxRows={2} size="small" />
+                        <TextField id='titleField' value={title} variant="outlined" multiline maxRows={2} size="small"
+                            onChange={(e) => setTitle(e.target.value)}
+                            sx={{ '& .MuiInputBase-input': { color: 'white' }, '& .MuiOutlinedInput-notchedOutline': { borderColor: 'gray' } }}/>
 
                         <p className="pt-2">Summary</p>
-                        <TextField id="summaryField" variant="outlined" multiline minRows={3} maxRows={6} size="small" />
+                        <TextField id="summaryField" value={summary} variant="outlined" multiline minRows={3} maxRows={6} size="small" 
+                            onChange={(e) => setSummary(e.target.value)}
+                            sx={{ '& .MuiInputBase-input': { color: 'white' }, '& .MuiOutlinedInput-notchedOutline': { borderColor: 'gray' } }}/>
                     </div>
-                    <div className="w-[200px] h-[200px] bg-gray-800">Pic</div>
+                    <div className="w-[400px] h-[240px] bg-gray-800">Pic</div>
                 </div>
 
                 {/* Rating */}
-                <div className="flex flex-row space-x-1 pt-4">
+                <div className="flex flex-row space-x-2 pt-4">
                     <p className="pt-[3px] pr-1">Rating:</p>
                     <p className="pt-[3px] w-[30px]">{rating.toString()}</p>
                     <FaRegStar className="pr-2" size={26} color="yellow" />
@@ -67,8 +78,38 @@ function CreateModal({ isOpen, onClose}: ModalProps) {
                 </div>
 
                 {/* Tags */}
-                <div className="flex flex-row space-x-1 pt-4">
-                    <p>Tags:</p>
+                <div className="flex flex-row space-x-2 pt-5">
+                    <p className="pt-[3px]">Tags:</p>
+                    <div className="space-x-1 space-y-1">
+                        {tags.map((tag) => (
+                            <button
+                                key={tag} 
+                                onClick={() => {
+                                    setSelected((prev) => 
+                                        prev.includes(tag) 
+                                            ? prev.filter((t) => t !== tag)
+                                            : [...prev, tag]
+
+                                    )
+                                }}
+                                className={`px-4 py-1 rounded-md text-sm ${selected.includes(tag)
+                                    ? "bg-blue-600 text-white"
+                                    : 'bg-gray-400 text-gray-800 hover:bg-gray-300'
+                                    }`}
+                            >
+                                {tag}
+                            </button>
+                    ))}
+
+                    </div>
+                </div>
+
+                {/* Review */}
+                <div className="flex flex-col pt-4">
+                    <p>Review</p>
+                    <TextField id='reviewField' value={review} variant="outlined" multiline minRows={4} maxRows={8} size="small" 
+                        onChange={(e) => setReview(e.target.value)}
+                        sx={{ '& .MuiInputBase-input': { color: 'white' }, '& .MuiOutlinedInput-notchedOutline': { borderColor: 'gray' } }}/>
                 </div>
 
                 {/* Close and create buttons */}
@@ -78,7 +119,11 @@ function CreateModal({ isOpen, onClose}: ModalProps) {
                         className="mt-4 px-3 py-1 bg-gray-500 rounded hover:bg-gray-600">
                         Close
                     </button>
-                    <button className="mt-4 px-3 py-1 bg-blue-500 rounded hover:bg-blue-600">
+                    <button 
+                        onClick={() => {
+                            /* Set up add review here */
+                        }} 
+                        className="mt-4 px-3 py-1 bg-blue-500 rounded hover:bg-blue-600">
                         Create
                     </button>
                 </div>
--- buchritter/src/server/api/review_req.ts
+++ buchritter/src/server/api/review_req.ts
@@ -4,6 +4,16 @@ import { Prisma, PrismaClient } from "@/server/prisma";
 
 const prisma = new PrismaClient();
 
+type Review = {
+  title: string;
+  summary: string;
+  rating: number;
+  review: string;
+  image_path?: string;
+  link?: string;
+  tags: number[];
+};
+
 export async function getReview(id: number) {
   return await prisma.reviews.findFirst({
     where: {
@@ -20,5 +30,24 @@ export async function getReviews() {
     include: {
       tags: true,
     },
+  });
+}
+
+export async function addReview(input: Review){
+  const { title, summary, rating, review, image_path, link, tags } = input;
+
+  return await prisma.reviews.create({
+    data: {
+      title: title,
+      summary: summary ?? '',
+      rating: rating,
+      review: review,
+      image_path: image_path,
+      link: link,
+      tags: {
+        connect: tags.map((id)=> ({ id })),
+      },
+    },
+    include: { tags: true },
   });
 }
